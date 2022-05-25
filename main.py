0396913162e8e0d123b2de78fa39964d5fdc340a
Shadyaobuya <shadyaobuyagard@gmail.com>
2022-05-25 22:59:48 +0300

    Added code to sort csv values

---
+++ main.py
@@ -0,0 +1,49 @@
+import csv
+
+class MergeSort():
+    def  __init__(self, data):
+        self.data = data
+
+    def mergeSort(data):
+        if len(data) > 1:
+            mid = len(data)//2
+            left_subArray = data[:mid]
+            right_subArray =  data[mid:]
+
+            MergeSort(left_subArray)
+            
+            MergeSort(right_subArray)
+
+            i = 0
+            j = 0
+
+            k = 0
+            while i < len(left_subArray) and j <len(right_subArray):
+                if left_subArray[i][9] < right_subArray[j][9]:
+                    data[k]= left_subArray[i]
+                    i+=1
+                else:
+                    data[k] = right_subArray[j]
+                    j+=1
+                k+=1
+
+            while i < len(left_subArray):
+                data[k] = left_subArray[i]
+                i+=1
+                k+=1
+
+            while j < len(right_subArray):
+                data[k] = right_subArray[j]
+                j+=1
+                k+=1
+
+
+
+
+csv_data = []
+with open('real-estate.csv', 'r') as file:
+            reader = csv.reader(file)
+            csv_data.extend(list(reader))
+
+sorted=MergeSort(csv_data)
+print(sorted)
